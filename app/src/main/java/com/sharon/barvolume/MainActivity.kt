0764dbfa80cc83e0801a934323f9f72d64f4a312
unknown <26laurasharon@gmail.com>
2020-10-06 11:19:43 +0700

    first commit

---
+++ app/src/main/java/com/sharon/barvolume/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.sharon.barvolume
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLenght: EditText
+    private lateinit var btnCalcuate: Button
+    private lateinit var tvResult: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLenght = findViewById(R.id.edt_legth)
+        btnCalcuate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalcuate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_calculate) {
+            val inputLength = edtLenght.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+            var isEmptyFields = false
+            if (inputLength.isEmpty()) {
+                isEmptyFields = true
+                edtLenght.error = "Field ini tidak boleh kosong"
+            }
+            if (inputWidth.isEmpty()) {
+                isEmptyFields = true
+                edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            if (inputHeight.isEmpty()) {
+                isEmptyFields = true
+                edtHeight.error = "Field ini tidak boleh kosong"
+            }
+            if (!isEmptyFields) {
+                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+                tvResult.text = volume.toString()
+            }
+        }
+    }
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT, tvResult.text.toString())
+    }
+}
